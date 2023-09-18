30b7173be0278a6d04ab1d2c249974f0a5148cff
codexhypra <kaeli7w7+2@gmail.com>
2023-09-18 22:25:59 +0100

    gallery

---
+++ assets/js/gallery.js
@@ -0,0 +1,21 @@
+function openImage(image) {
+    const modal = document.getElementById("modal");
+    const fullscreenImage = document.getElementById("fullscreen-image");
+
+    // Set the image source in the modal
+    fullscreenImage.src = image.src;
+
+    // Extract and display the filename without the extension
+    const filenameContainer = image.parentElement.querySelector(".filename");
+    const filenameWithExtension = image.alt; // Assuming alt attribute contains the filename with extension
+    const filenameWithoutExtension = filenameWithExtension.replace(/\.[^/.]+$/, ''); // Remove the extension
+    filenameContainer.textContent = filenameWithoutExtension;
+
+    // Display the modal
+    modal.style.display = "block";
+}
+
+function closeImage() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}

efafaa40278c7da1f97087979fb376cc1a28b157
codexhypra <kaeli7w7+2@gmail.com>
2023-09-20 00:27:58 +0100

    esc

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -20,9 +20,19 @@ function openImage(image) {
             closeImage();
         }
     });
+
+    // Add a keydown event listener to close the modal when "ESC" key is pressed
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+            closeImage();
+        }
+    });
 }
 
 function closeImage() {
     const modal = document.getElementById("modal");
     modal.style.display = "none";
+
+    // Remove the keydown event listener when the modal is closed
+    document.removeEventListener("keydown", closeImage);
 }
